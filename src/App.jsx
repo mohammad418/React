152c41bb0cba1340d209e668c3aaf8e630dc6313
mohammad <mnloo573@gmail.com>
2023-08-13 12:31:11 +0330

    completed mini project

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import "./index.css"
 function App(){
     return(
         <div>
-            <div className="container">
+            <div>
                 <h1>My Courses (3)</h1> 
                 <div className="course-status">
-                    <p>All</p>
-                    <p className="Active" >Active</p>
-                    <p>Completed</p>
-                    <p>UpComing</p>
+                    <div>All</div>
+                    <div>Active</div>
+                    <div>Completed</div>
+                    <div>UpComing</div>
                 </div>  
             </div>
             <div className="course-list">
@@ -45,8 +45,84 @@ function App(){
                     </div>
                    
                 </div>
-                <div className="course-item"></div>
-                <div className="course-item"></div>
+                <div className="course-item">
+                    <div className="course-item__img">
+                        <img src="/public/images/img2.jpg" alt="" className="img" />
+                    </div>
+                    <div className="course-item__detail">
+                        <div className="course-item__body">
+                            <div>
+                                <p className="title">
+                                    Design Strategy
+                                </p>
+                                <p className="desc">
+                                    Lesson On Planning Design Concept And Proper Planning Of Work
+                                </p>
+                            </div>
+                            
+                            <span className="rate">
+                                ⭐️
+                                3.9
+                            </span>
+                        </div>
+                        <div className="course-item__footer">
+                            <div className="tags">
+                                <span className="badge badge--secondary">
+                                    React.js
+                                </span>
+                                <span className="badge badge--secondary">
+                                    React.js
+                                </span>
+                            </div>
+                            <div className="caption">
+                                <div className="date">
+                                    Aug 4 , 2023
+                                </div>
+                                <span className="badge badge--danger">
+                                    UpComing
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="course-item">
+                    <div className="course-item__img">
+                        <img src="/public/images/img3.jpg" alt="" className="img" />
+                    </div>
+                    <div className="course-item__detail">
+                        <div className="course-item__body">
+                            <div>
+                                <p className="title">Business Lecture</p>
+                                <p className="desc">Lecturs On How To Build Your Business Safely Without Fare Of New Projects</p>
+                            </div>
+                            <span className="rate">
+                                ⭐️
+                                4.2
+                            </span>
+                        </div>
+                        <div className="course-item__footer">
+                            <div className="tags">
+                                <span className="badge badge--secondary">
+                                    React.js
+                                </span>
+                                <span className="badge badge--secondary">
+                                    Node.js
+                                </span>
+                            </div>
+                            <div className="caption">
+                                <div className="date">
+                                    Jul 2 , 2023
+                                </div>
+                                <span className="badge badge--primary">
+                                    Active
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <p>Course Result:</p>
+                <p>You have successfully passed 33% of your courses.</p>
             </div>
         </div>
     )
